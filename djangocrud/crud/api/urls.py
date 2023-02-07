62a52a27e096275e4e8844a362a5351208849d45
AtaharNurPial <atp82232@gmail.com>
2023-02-07 13:18:32 +0600

    feat: added api authentication with JWT Token

--- djangocrud/crud/api/urls.py
+++ djangocrud/crud/api/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
+from rest_framework_simplejwt.views import token_obtain_pair, token_refresh
 from . import views
 
 urlpatterns = [
     path('', views.getRoutes),
     path('rooms/', views.getRooms),
     path('rooms/<str:room_id>', views.getRoomById),
-    path('get-token/', obtain_auth_token, name='get-token'),
+    path('get-token/', token_obtain_pair, name='token_obtain_pair'),
+    path('get-token-refresh/', token_refresh, name='token_refresh'),
 ]
